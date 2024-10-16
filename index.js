319fa77a900be4ec58df1055f7576efa77062b58
khan12ok <skshimul79977007@gmail.com>
2024-10-16 10:04:59 -0700

    new1

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+const  express=require('express')
+const db=require('./Datalocation/db')
+const app=express()
+const cors=require('cors')
+const port=8000
+app.use(express.json())
+const coursOption={
+origin:" http://localhost:5173 ",
+method:"GET,POST,PUT,DELETE,PATCH,HEAD   ",
+credentials:true,
+}
+
+const reg=require('./Skima/Peraon')
+
+//const router=require('./router/auth-router')
+
+app.use(cors(coursOption))
+//app.use("/app/auth",router)
+
+app.get('/hell', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.post('/res',async(req,res)=>{
+
+  try {
+    const {name,email,password}=req.body
+    const datafind=await reg.findOne({email})
+
+    if(datafind){
+      return res.status(200).json("this is hvend")
+    }
+    
+   const usercreate=await reg.create({
+    name,email,password
+   }) 
+   console.log(usercreate)
+   res.status(201).json({msg:usercreate})
+
+  } catch (error) {
+    console.log(error)
+  }
+
+
+})
+
+
+
+db().then(()=>{
+
+  app.listen(port,()=>{
+
+   console.log(`surver is running at port:${port}`)    
+
+  })
+
+})
